7c3efd8ba6cf205d463adf77c96a3fa70f704cc6
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2024-09-03 15:26:01 +0200

    Update Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,8 +5,11 @@ import { getFirestore, collection, query, getDocs } from "firebase/firestore";
 import "../style/Order.css";
 
 const Order = () => {
+  //use this for dev local
   const STRIPE_SECRET1 = process.env.REACT_APP_STRIPE_SECRET;
-  const stripe = require("stripe")(STRIPE_SECRET);
+
+  //change to process.env.STRIPE_SECRET for prod
+  const stripe = require("stripe")(process.env.STRIPE_SECRET);
   const [cEmail, setcEmail] = useState();
   const [cTotal, setcTotal] = useState();
   const [cItems, setcItems] = useState([]);
