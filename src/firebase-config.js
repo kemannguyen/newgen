e88a3e5d8c826deab9370e09486bbbe5c868df32
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2024-09-12 20:41:07 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,8 +10,9 @@ import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const FIREBASE_SECRET = process.env.FIREBASE_APIKEY;
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
+  apiKey: FIREBASE_SECRET,
   authDomain: "newgen-1309c.firebaseapp.com",
   projectId: "newgen-1309c",
   storageBucket: "newgen-1309c.appspot.com",
