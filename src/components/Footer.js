1d28c7b1a6a4ebd898f625505a08d4bbe9516edd
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2024-09-17 15:21:53 +0200

    feat: basic footer layout
    implemented buttons, design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,19 +15,41 @@ const Footer = () => {
         <div className="footer-container">
           <div className="section-text">
             <div>COSTUMER CARE</div>
-            <div>contact us</div>
-            <div>my order</div>
-            <div>payment</div>
-            <div>contact us</div>
+            <div className="section">
+              <div className="section-btns">
+                <a href="/order" className="section-btn">
+                  Contact us
+                </a>
+              </div>
+              <div className="section-btns">
+                <a href="/order" className="section-btn">
+                  My order
+                </a>
+              </div>
+              <div className="section-btn">Payment</div>
+            </div>
           </div>
           <div className="section-text">
             <div>SHIPPING & RETURNS </div>
+            <div className="section">
+              <div className="section-btns">
+                <a href="/order" className="section-btn">
+                  Track my order
+                </a>
+              </div>
+              <div className="section-btns">Shipping options</div>
+              <div className="section-btns">Returns</div>
+            </div>
           </div>
           <div className="section-text">
             <div>OUR COMPANY </div>
+            <div className="section">
+              <div className="section-btns">About us</div>
+            </div>
           </div>
         </div>
         <div
+          className="watermark"
           style={{
             textAlign: "center",
           }}
